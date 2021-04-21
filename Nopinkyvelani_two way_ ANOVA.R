a91bdd30f213814005887865b95f3beb06c72730
3769148@myuwc.ac.za <3650499@myuwc.ac.za>
2021-04-21 08:18:53 +0200

    Nopinkyvelani_two way_ANOVA

---
+++ Nopinkyvelani_two way_ ANOVA.R
@@ -0,0 +1,76 @@
+#Nopinky Velani
+#Two type ANOVA
+#chapter7
+#21/04/2021
+
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+library(readr)
+
+
+snakes <- read_csv("C:/Users/BCB User/Desktop/R data/R_Work/Intro_R/snakes.csv")
+
+snakes$day = as.factor(snakes$day)
+
+snakes.summary <- snakes %>% 
+  group_by(day, snake) %>% 
+  summarise(mean_openings = mean(openings),
+            sd_openings = sd(openings)) %>% 
+  ungroup()
+snakes.summary
+  ungroup()
+snakes.summary
+
+snakes.summary <- snakes %>% 
+  group_by(day) %>% 
+  summarise(mean_openings = mean(openings),
+            sd_openings = sd(openings)) %>% 
+  ungroup()
+snakes.summary
+
+library(Rmisc)
+snakes.summary2 <- summarySE(data = snakes, measurevar = "openings", groupvars = c("day"))
+
+            
+ggplot(data = snakes, aes(x = day, y = openings)) +
+  geom_segment(data = snakes.summary2, aes(x = day, xend = day, y = openings - ci, yend = openings + ci, colour = day),
+               size = 2.0, linetype = "solid", show.legend = F) +
+  geom_boxplot(aes(fill = day), alpha = 0.6, show.legend = F) + 
+  geom_jitter(width = 0.05)
+snakes.aov <- aov(openings ~ day + snake, data = snakes)
+summary(snakes.aov)
+
+
+par(mfrow = c(2, 2))
+# Checking assumptions...
+# make a histogram of the residuals;
+# they must be normal
+snakes.res <- residuals(snakes.aov)
+hist(snakes.res, col = "red")
+
+# make a plot of residuals and the fitted values;
+# # they must be normal and homoscedastic
+plot(fitted(snakes.aov), residuals(snakes.aov), col = "red")
+
+snakes.tukey <- TukeyHSD(snakes.aov, which = "day", conf.level = 0.90)
+plot(snakes.tukey, las = 1, col = "red")  
+
+
+# Nwe graph codes ---------------------------------------------------------
+
+ggplot(data = snakes, aes(x = day, y = openings)) +
+  geom_segment(data = snakes.summary2, aes(x = day, xend = day, y = openings - ci, yend = openings + ci, colour = day),
+               size = 2.0, linetype = "solid", show.legend = F) +
+  plot(fitted(snakes.aov), residuals(snakes.aov), col = "red")
+
+
+ggplot(data = snakes, aes(x = day, y = openings)) +
+  geom_segment(data = snakes.summary2, aes(x = day, xend = day, y = openings - ci, yend = openings + ci, colour = day),
+               size = 2.0, linetype = "solid", show.legend = F) +
+  plot(snakes.tukey, las = 1, col = "red") 
+  
+
+
+
+  
